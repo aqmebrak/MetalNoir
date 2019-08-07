1c2e24ac50f421847f1eb9bc538127018aeeb6e1
MatthieuPe <matthieu.perrin@gokepler.fr>
2019-08-07 10:29:07 +0200

    add p5js

--- components/Processing.vue
+++ components/Processing.vue
@@ -2,3 +2,17 @@
   <div id="container"></div>
 </template>
 
+<script>
+if (process.client) {
+  const p5 = require('p5')
+  const sketch = function(p) {
+    p.setup = function() {
+      p.createCanvas(500, 500)
+      p.background(255)
+    }
+  }
+
+  // eslint-disable-next-line no-new,new-cap
+  new p5(sketch, 'container')
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,22 @@
 <template>
   <div class="container">
     <div>
-      <logo />
+      <Processing />
       <h1 class="title">
         metalnoir
       </h1>
       <h2 class="subtitle">
         Black Metal project
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 import Processing from '~/components/Processing.vue'
 
 export default {
   components: {
-    Logo,
     Processing
   }
 }
@@ -44,6 +30,7 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  background-color: #35495e;
 }
 
 .title {
@@ -52,14 +39,14 @@ export default {
   display: block;
   font-weight: 300;
   font-size: 100px;
-  color: #35495e;
+  color: #f5f5f5;
   letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
   font-size: 42px;
-  color: #526488;
+  color: #f5f5f5;
   word-spacing: 5px;
   padding-bottom: 15px;
 }
